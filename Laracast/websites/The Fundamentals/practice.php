70288f2936f7e64276f7a49099e127d7b8d79cc2
asgardbattle-ux <asgard.battle@gmail.com>
2026-07-26 19:50:14 +0300

    Fundamentals quiz practice

--- Laracast/websites/The Fundamentals/practice.php
+++ Laracast/websites/The Fundamentals/practice.php
@@ -9,35 +9,12 @@
 
 <body>
     <?php
-    $books = [
-        [
-            'name' => 'Do Androids Dream of Electric Sheep',
-            'author' => 'Philip K. Dick',
-            'releaseYear' => 1968,
-            'purchaseUrl' => 'http://example.com',
-            'category' => 'fantasy'
-        ],
-        [
-            'name' => 'Porject Hail Mary',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2021,
-            'purchaseUrl' => 'http://example.com',
-            'category' => 'crime'
-        ],
-        [
-            'name' => 'The Martian',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2011,
-            'purchaseUrl' => 'http://example.com',
-            'category' => 'mistery'
-        ]
-    ];
-    $printVariable = 'releaseYear';
-
-    foreach ($books as $bookInfo) {
-        echo $bookInfo[$printVariable] . '<br>';
+    function multiply(int $a, int $b)
+    {
+        return $a * $b;
     }
 
+    echo multiply("5", 3);
     ?>
 </body>
 
